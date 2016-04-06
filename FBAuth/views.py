0b7e083407c8734d8310dfb80b1bc54e2dc5d6b0
JaeHoon Lee <jaehoonlee88@gmail.com>
2016-04-06 17:13:57 -0400

    merge

--- FBAuth/views.py
+++ FBAuth/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render_to_response
 from django.template.context import RequestContext
+from social_auth.models import *
+
 
 def home(request):
-   context = RequestContext(request, {'request':request, 'user': request.user})
-   return render_to_response('social/home.html', context_instance=context)
+    request.user.social_auth
+    context = RequestContext(request, {'request':request, 'user': request.user})
+    return render_to_response('social/home.html', context_instance=context)
