3800b4abfc52ba997e2690cf4082e2095e13e933
jaehoonLee <jaehoonlee88@gmail.com>
2016-04-06 14:11:28 -0700

    social auth

--- FBAuth/views.py
+++ FBAuth/views.py
@@ -2,5 +2,11 @@ from django.shortcuts import render_to_response
 from django.template.context import RequestContext
 
 def home(request):
+   #social = request.user.social_auth.get(provider='facebook-oauth2')
+   if not request.user.is_anonymous():
+      print request.user.social_auth.get(provider='facebook').extra_data['access_token']
+   
+
+
    context = RequestContext(request, {'request':request, 'user': request.user})
    return render_to_response('social/home.html', context_instance=context)
